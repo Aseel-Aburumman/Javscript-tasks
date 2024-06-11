ed816eedad56ca446740aff33e4f55baabe9bc64
Aseel Aburumman <aseelaburumman49@gmail.com>
2024-06-11 12:36:43 +0300

    ya rab

--- orange/orange.js
+++ orange/orange.js
@@ -22,122 +22,119 @@ let PasswordError2 = document.getElementById("passHelp2");
 
 let thecheck = document.getElementById("exampleCheck1");
 
-let usernamevalid = false;
-let emailvalid = false;
-let mobilevalid = false;
-let passvalid = false;
-let passvalid2 = false;
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-form.addEventListener("input", thevalidation_process);
-function thevalidation_process() {
+  let usernamevalid = validateUsername();
+  let emailvalid = validateEmail();
+  let mobilevalid = validateMobile();
+  let passvalid = validatePassword();
+  let passvalid2 = validateConfirmPassword();
+
+  if (usernamevalid && emailvalid && mobilevalid && passvalid && passvalid2) {
+    const userObj = {
+      username: username.value,
+      email: email.value,
+      Password: Password.value,
+      mobile: mobile.value,
+    };
+    localStorage.setItem("user", JSON.stringify(userObj));
+  }
+});
+
+function validateUsername() {
   let usernamevalue = username.value;
   let usernamevalidation_result = usernamevalidation.test(usernamevalue);
 
   if (usernamevalue === "") {
     usernameerror.style.color = "red";
-    usernamevalid = false;
-
-    usernameerror.innerHTML = "this feild is required";
+    usernameerror.innerHTML = "this field is required";
+    return false;
   } else if (usernamevalidation_result) {
     usernameerror.style.color = "green";
-    usernamevalid = true;
-
     usernameerror.innerHTML = "this is perfect";
+    return true;
   } else {
     usernameerror.style.color = "red";
-    usernamevalid = false;
-
-    usernameerror.innerHTML = "this is not valid username format ";
+    usernameerror.innerHTML = "this is not valid username format";
+    return false;
   }
+}
 
+function validateEmail() {
   let emailvalue = email.value;
   let emailvalidation_result = emailvalidation.test(emailvalue);
 
   if (emailvalue === "") {
     emailerror.style.color = "red";
-    emailvalid = false;
-
-    emailerror.innerHTML = "this feild is required";
+    emailerror.innerHTML = "this field is required";
+    return false;
   } else if (emailvalidation_result) {
     emailerror.style.color = "green";
-    emailvalid = true;
-
     emailerror.innerHTML = "this is perfect";
+    return true;
   } else {
     emailerror.style.color = "red";
-    emailvalid = false;
-
-    emailerror.innerHTML = "this is not valid email format ";
+    emailerror.innerHTML = "this is not valid email format";
+    return false;
   }
+}
 
+function validateMobile() {
   let mobilevalue = mobile.value;
   let mobilevalidation_result = mobilevalidation.test(mobilevalue);
 
   if (mobilevalue === "") {
     mobileError.style.color = "red";
-    mobilevalid = false;
-    mobileError.innerHTML = "this feild is required";
+    mobileError.innerHTML = "this field is required";
+    return false;
   } else if (mobilevalidation_result) {
     mobileError.style.color = "green";
-    mobilevalid = true;
-
     mobileError.innerHTML = "this is perfect";
+    return true;
   } else {
     mobileError.style.color = "red";
-    mobilevalid = false;
-
     mobileError.innerHTML =
-      "this is not valid number , make sure its start with 077 and contain 10 diget";
+      "this is not valid number, make sure it starts with 077 and contains 10 digits";
+    return false;
   }
+}
 
+function validatePassword() {
   let Passwordvalue = Password.value;
   let Passwordvalidation_result = Passwordvalidation.test(Passwordvalue);
 
   if (Passwordvalue === "") {
     PasswordError.style.color = "red";
-    passvalid = false;
-    PasswordError.innerHTML = "this feild is required";
+    PasswordError.innerHTML = "this field is required";
+    return false;
   } else if (Passwordvalidation_result) {
     PasswordError.style.color = "green";
-    passvalid = true;
     PasswordError.innerHTML = "this is perfect";
+    return true;
   } else {
     PasswordError.style.color = "red";
-    passvalid = false;
     PasswordError.innerHTML =
-      "this is not strong Password , make sure its  Between 6 and 18 characters, has at least one special character, has at least one lowercase letter, one uppercase letter, and one digit  ";
+      "this is not a strong password, make sure it's between 6 and 18 characters, has at least one special character, one lowercase letter, one uppercase letter, and one digit";
+    return false;
   }
+}
 
+function validateConfirmPassword() {
+  let Passwordvalue = Password.value;
   let Passwordvalue2 = Password2.value;
 
   if (Passwordvalue2 === "") {
     PasswordError2.style.color = "red";
-    passvalid2 = false;
-    PasswordError2.innerHTML = "this feild is required";
+    PasswordError2.innerHTML = "this field is required";
+    return false;
   } else if (Passwordvalue2 === Passwordvalue) {
     PasswordError2.style.color = "green";
-    passvalid2 = true;
-    PasswordError2.innerHTML = "good job ";
+    PasswordError2.innerHTML = "good job";
+    return true;
   } else {
     PasswordError2.style.color = "red";
-    passvalid2 = false;
-    PasswordError2.innerHTML = "passwords do not match ";
-  }
-
-  let alertShown = false;
-  let thesubmitBtn = document.getElementById("submitBtn");
-  thesubmitBtn.addEventListener("click", submitTheData);
-  function submitTheData() {
-    event.preventDefault();
-    if (usernamevalid && passvalid && mobilevalid && emailvalid && passvalid2) {
-      const userObj = {
-        username: usernamevalue,
-        email: emailvalue,
-        Password: Passwordvalue,
-        mobile: mobilevalue,
-      };
-      localStorage.setItem("user", JSON.stringify(userObj));
-    } else {
-    }
+    PasswordError2.innerHTML = "passwords do not match";
+    return false;
   }
 }
