5382816c9416af8befeaba9122357b14baf75b77
Aseel Aburumman <aseelaburumman49@gmail.com>
2024-06-12 12:21:49 +0300

    hehe

--- dark theme/dark.js
+++ dark theme/dark.js
@@ -1,22 +1,29 @@
-function toggleMode() {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
+function DarkMode() {
+  const body = document.body;
+  const hehe = document.getElementById("hehe");
+  body.classList.toggle("dark-mode");
 
-    if (body.classList.contains('dark-mode')) {
-        sessionStorage.setItem('mode', 'dark');
-    } else {
-        sessionStorage.setItem('mode', 'light');
-    }
+  if (body.classList.contains("dark-mode")) {
+    sessionStorage.setItem("mode", "dark");
+    hehe.style.display = "block";
+  } else {
+    sessionStorage.setItem("mode", "light");
+    hehe.style.display = "none";
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const savedMode = sessionStorage.getItem('mode');
-    if (savedMode === 'dark') {
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.add('light-mode');
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const hehe = document.getElementById("hehe");
 
-    const toggleButton = document.getElementById('toggleMode');
-    toggleButton.addEventListener('click', toggleMode);
+  const savedMode = sessionStorage.getItem("mode");
+  if (savedMode === "dark") {
+    document.body.classList.add("dark-mode");
+    hehe.style.display = "block";
+  } else {
+    document.body.classList.add("light-mode");
+    hehe.style.display = "none";
+  }
+
+  const toggleButton = document.getElementById("toggleMode");
+  toggleButton.addEventListener("click", DarkMode);
 });
